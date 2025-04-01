6001756d20998c7765bf162abe1253993e42b3d7
Bernhard Urban-Forster <lewurm@gmail.com>
2025-04-01 23:38:46 +0200

    delete old polyglot demo

--- poly/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-# $JAVA_HOME/bin/node --engine.TraceCompilation --jvm --polyglot server.js
-$JAVA_HOME/bin/node --jvm --polyglot server.js
-# $JAVA_HOME/bin/node --native --polyglot server.js
--- poly/str_wrapper.py
+++
@@ -1,5 +0,0 @@
-import polyglot
-
-@polyglot.export_value
-def wrap_with_color(color, body):
-    return "<p style=\"color:{}\";>{}</p> <br />".format(color, body)
